d81655c5c955d52c62115eaeee08061328bee88e
Owi27 <owens.nicolas02@icloud.com>
2023-07-10 17:05:48 -0400

    Updated Path

--- url-shortener-server/app/main.py
+++ url-shortener-server/app/main.py
@@ -3,7 +3,9 @@ import firebase_admin
 from firebase_admin import db
 import os
 
-cred_obj = firebase_admin.credentials.Certificate("../ServiceAccountKey.json")
+cred_obj = firebase_admin.credentials.Certificate(
+    "./url-shortener-server/ServiceAccountKey.json"
+)
 default_app = firebase_admin.initialize_app(
     cred_obj,
     {"databaseURL": "https://url-shortener-bcfd8-default-rtdb.firebaseio.com/"},
